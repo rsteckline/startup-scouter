103206fa2232c541dbea7cce731022e4df53f73f
Bobby Steckline <rjsturing@gmail.com>
2024-01-10 12:41:24 -0500

    Add api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,33 @@
+export const getJobIds = async () => {
+  try {
+    const response = await fetch(
+      "https://hacker-news.firebaseio.com/v0/jobstories.json"
+    );
+    if (!response.ok) {
+      throw new Error(
+        `Error fetching job stories: ${response.status} ${response.statusText}`
+      );
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching job story IDs:", error);
+    throw error;
+  }
+};
+
+export const getJobDetails = async (id) => {
+  try {
+    const response = await fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+    );
+    if (!response.ok) {
+      throw new Error(
+        `Error fetching job story details: ${response.status} ${response.statusText}`
+      );
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching job story with ID ${id}:`, error);
+    throw error;
+  }
+};
