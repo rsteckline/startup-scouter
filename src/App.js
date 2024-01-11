d70343aa6496fbfe09915c1a4844e7cb3c5cf9b1
Bobby Steckline <rjsturing@gmail.com>
2024-01-10 19:50:36 -0500

    Update favicon and page title

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
-x
+import React from 'react';
+import './App.css';
+import MainPage from './components/MainPage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Startup Scouter</h1>
+      </header>
+      <main>
+        <MainPage />
+      </main>
+    </div>
+  );
+}
+
+export default App;
